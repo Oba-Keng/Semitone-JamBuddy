047ddc92c9d216a9a8d1ad1b51d9ebb013ece4b9
Obakeng <obeezus@gmail.com>
2020-01-10 13:44:46 +0200

    3rd commit

--- notecircle.js
+++ notecircle.js
@@ -1,61 +1,66 @@
 class JamBuddy {
+  constructor() {
+    this.notes = [
+      "A",
+      "A#",
+      "B",
+      "C",
+      "C#",
+      "D",
+      "D#",
+      "E",
+      "F",
+      "F#",
+      "G",
+      "G#"
+    ];
+    this.finalArray;
+    this.finalDigit;
+    this.fArray;
+    this.userAnswer;
+  }
 
-    constructor(){
-        this.notes = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
-        this.finalArray;
-        this.finalDigit;
+  // function that returns two random notes in a notecircle
+  selectNotes() {
+    let newNotes = [];
+    newNotes.push(...this.notes);
+
+    let randomArray = newNotes.sort(() => Math.random() - 0.5);
+
+    this.finalArray = randomArray.slice(0, 2);
+    this.finalArray.sort();
+
+    document.getElementById("pFinalArray").innerHTML = this.finalArray;
+
+    return this.finalArray;
+  }
+
+  //function returns the index of selected notes
+  checkAnswer() {
+    let indexOne = this.notes.indexOf(this.finalArray[0]);
+
+    let indexTwo = this.notes.indexOf(this.finalArray[1]);
+
+    this.finalDigit = indexOne - indexTwo;
+
+    //converts negative numbers by multiplying the number by one
+
+    this.finalDigit < 0 ? (this.finalDigit *= -1) : this.finalDigit;
+
+    this.userAnswer = document.getElementById("answer").value;
+
+    //checks if user input is the correct answer
+    if (this.userAnswer == this.finalDigit) {
+      return (document.getElementById("p1").innerHTML =
+        "You got it right budd,well Done!");
+    } else {
+      return (document.getElementById("p1").innerHTML =
+        "Wrong answer! Try again");
     }
-
-// function that returns two random notes in a notecircle
-    selectNotes(){
-
-        
-        let newNotes = []
-        newNotes.push(...this.notes);
-        let originalArray = document.getElementById('pArray').innerHTML=newNotes;
-        
-        let randomArray = newNotes.sort(()=>Math.random()-0.5)
-        let rArray = document.getElementById('pRandomArray').innerHTML=randomArray;
-        
-        this.finalArray = randomArray.slice(0,2);
-        this.finalArray.sort();
-        let fArray = document.getElementById('p').innerHTML=this.finalArray;
-
-        return originalArray,rArray,fArray;
-
-    }
-
-//function returns the index of selected notes
-    checkAnswer(){
-
-        let indexOne = this.notes.indexOf(this.finalArray[0])
-                   
-        let indexTwo = this.notes.indexOf(this.finalArray[1])
-
-                  this.finalDigit = indexOne - indexTwo;
-
-//converts negative numbers by multiplying the number by one
-
-        this.finalDigit < 0 ? this.finalDigit *=-1 : this.finalDigit
-            
-        let userAnswer = document.getElementById('answer').value;
-        
-
-        if(userAnswer==this.finalDigit){
-
-            return document.getElementById('p1').innerHTML="You got it right budd,well Done!";
-           
-            
-
-        }else {
-            return document.getElementById('p1').innerHTML="Wrong answer! Try again";
-        }    
-    
+  }
 }
-}
-// module.exports = JamBuddy;
+module.exports = JamBuddy;
 
- jam = new JamBuddy();
- notes = jam.selectNotes();
- notice = jam.checkAnswer()
-
+// jam = new JamBuddy();
+// notes = jam.selectNotes();
+// notice = jam.checkAnswer();
--- spec/notecircle.spec.js
+++ spec/notecircle.spec.js
@@ -1,19 +1,28 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const JamBuddy = require("../notecircle")
+const JamBuddy = require("../notecircle");
 
-describe("", function(){
+describe("", function() {
+  beforeEach(function() {
+    const dom = new jsdom.JSDOM(
+      '<html><body><div id="pFinalArray"></div></body></html>',
+      '<html><body><div><input id="answer"></input></div></body></html>'
+    );
+    global.document = dom.window.document;
+    global.window = dom.window;
+    global.navigator = dom.window.navigator;
+  });
 
-    it('selects notes', function(){
-        let buddy = new JamBuddy();
-expect(buddy.selectNotes()).toEqual(jasmine.any(Array))
+  it("select notes", function() {
+    let buddy = new JamBuddy();
 
-    });
+    expect(buddy.selectNotes()).toEqual(jasmine.any(Array));
+  });
 
-    it('checks notes', function(){
-        let buddy = new JamBuddy();
-        expect(buddy.checkAnswer(this.finalDigit)).toEqual(true)
-    });
+  it("checks notes", function() {
+    let buddy = new JamBuddy();
 
-})
+    expect(buddy.checkAnswer()).toEqual(jasmine.any(Number));
+  });
+});
